1247f85b6ce593f8b97a145868208046c124e0a4
Kotaro Suto <ng1a5ss43@gmail.com>
2019-05-24 02:19:51 +0900

    Add hello.c

---
+++ hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main (void);
+
+int main(void)
+{
+	printf("このコンパイラが・・・・");
+	printf("\n");
+
+	printf("char min = %d\n", CHAR_MIN);
+	printf("char max = %d\n", CHAR_MAX);
+	printf("\n");
+
+	return 0;
+}

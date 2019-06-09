5ffa48ad7f5c7868c88bdff988cd62ac1d062c3f
KS1019 <ng1a5ss43@gmail.com>
2019-06-10 00:26:24 +0900

    Start doing a0302.c

---
+++ a0302.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 256
+#define NAME_SIZE 256
+
+int main(void);
+void get_line(char *buffer, int size);
+
+int main(void) {
+    char name1[NAME_SIZE];
+    char name2[NAME_SIZE];
+    char buffer[BUFFER_SIZE];
+    double age1, age2;
+
+    printf("２人の平均年齢を計算します。\n");
+
+    printf("1人目の名前を入力してください。\n");
+
+    
+}
